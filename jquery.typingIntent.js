ea0fcbcbc5e8edf4e75d3cf55b36900d442d3e5e
Amiel Martin <amiel@carnesmedia.com>
2012-01-31 16:34:42 -0800

    Fix whitespace and have a better comment

--- jquery.typingIntent.js
+++ jquery.typingIntent.js
@@ -1,32 +1,47 @@
 /*
- * jquery.typingIntent.js
+ * jQuery typing intent plugin
  * Amiel Martin
- * 2010-10-28
+ * Carnes Media
+ * 2012-01-19
  *
- * Fire an event after the user has finished typing.
+ * Runs a callback not just on keyup, but after a user has typed something.
+ * The idea is similar to hoverIntent.
+ *
+ * Examples:
+ *
+ *    $('input#url').typing_intent(function() {
+ *      $.get('/validate_url', { url: this.value });
+ *    });
+ *
+ *    var ajax;
+ *    $('input#url').typing_intent(function() {
+ *      ajax = $.get('/validate_url', { url: this.value });
+ *    }, function() {
+ *      ajax.abort();
+ *    }, { timeout: 2000 });
+ *
  */
 
-
-
 ;(function($) {
-	var ti;
-	
-	ti = $.fn.typingIntent = function(callback, immediate_callback, user_options) {
-		var timer, options = $.extend({}, ti.default_options, user_options);
-            
-		var handler = function(e) {
-			var self = this;
-			if (typeof immediate_callback === "function") {
-				if ( immediate_callback.apply(self, [e]) === false ) return;
-			}
-			clearTimeout(timer);
-			timer = setTimeout(function() { callback.apply(self, [e]); }, options.timeout);
-		};
-      
-		return this.keyup(handler);
-	};
-	
-	ti.default_options = {
-		timeout: 500
-	};
+    var ti;
+
+    ti = $.fn.typing_intent = function(callback, immediate_callback, user_options) {
+        var timer, options = $.extend({}, ti.default_options, user_options);
+
+        var handler = function(e) {
+            var self = this;
+            if (typeof immediate_callback === "function") {
+                if ( immediate_callback.apply(self, [e]) === false ) return;
+            }
+            clearTimeout(timer);
+            timer = setTimeout(function() { callback.apply(self, [e]); }, options.timeout);
+        };
+
+        return this.keyup(handler);
+    };
+
+    ti.default_options = {
+        timeout: 1500
+    };
 })(jQuery);
+
